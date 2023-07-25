a84f74da40b0a5c95a95e2ba21fa625b9006614b
akash <aregret01@gmail.com>
2023-07-25 19:59:34 +0530

    added templates

--- users/views.py
+++ users/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def register(request): #forget the "request" argument here.....
-    return HttpResponse("Register")    
+  #changed return HttpResponse("register") to this ...
+  return render(request,'users/register.html')
 
 
 def login(request):
-    return HttpResponse("Login")
+    return render(request,'users/login.html')
 
 def homePage(request):
-    return HttpResponse("Home")
+    return render(request,'users/index.html')
 
 def aboutPage(request):
-    return HttpResponse("About")
+    return render(request,'users/about.html')
